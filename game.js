80b4a270979ae1c3ef87fe064a66f96a7aea7b3d
S P Wright <7489235+sp-wright@users.noreply.github.com>
2020-05-08 17:55:40 +0100

    Add files via upload
    larger icons on mobile

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var buttonColors = ["red", "blue", "green", "yellow"];
+var buttonColors = [ 'red', 'blue', 'green', 'yellow' ];
 var gamePattern = [];
 var userClickedPattern = [];
 var level = 0;
@@ -7,83 +7,82 @@ var highScore = 0;
 /////////////////////////// SEQUENCE CREATOR
 
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
-  $("#" + randomChosenColor).fadeOut(50).fadeIn(50);
-  sounds(randomChosenColor);
-  level++;
-  $("#level-title").text("Level " + level);
-  if(level>highScore){
-    highScore++;
-  }
-  $(".high-score").text("High Score = " + highScore);
+	var randomNumber = Math.floor(Math.random() * 4);
+	var randomChosenColor = buttonColors[randomNumber];
+	gamePattern.push(randomChosenColor);
+	$('#' + randomChosenColor).fadeOut(50).fadeIn(50);
+	sounds(randomChosenColor);
+	level++;
+	$('#level-title').text('Level ' + level);
+	if (level > highScore) {
+		highScore++;
+	}
+	$('.high-score').text('High Score = ' + highScore);
 }
 
 /////////////////////////// USER INPUTS
 
-$(".start-btn").click(function() {
-  if (level === 0) {
-    nextSequence();
-  }
-  $(".start-btn").hide();
+$('.start-btn').click(function() {
+	if (level === 0) {
+		nextSequence();
+	}
+	$('.start-btn').hide();
 });
 
-$(".btn").click(function(event) {
-  var userChosenColor = event.currentTarget.id;
-  userClickedPattern.push(userChosenColor);
-  sounds(userChosenColor);
-  animatePress(userChosenColor);
-  checkAnswer(userClickedPattern.length - 1);
+$('.btn').click(function(event) {
+	var userChosenColor = event.currentTarget.id;
+	userClickedPattern.push(userChosenColor);
+	sounds(userChosenColor);
+	animatePress(userChosenColor);
+	checkAnswer(userClickedPattern.length - 1);
 });
 
 /////////////////////////// RULES
 
-$(".rules").hide();
+$('.rules').hide();
 
-$(".rules-btn").click(function(){
-  $(".rules").slideToggle();
+$('.rules-btn').click(function() {
+	$('.rules').slideToggle();
 });
-
 
 /////////////////////////// VERIFICATION
 
 function checkAnswer(currentLevel) {
-  if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
-    if (userClickedPattern.length == gamePattern.length) {
-      setTimeout(function() {
-        nextSequence()
-      }, 1000)
-      userClickedPattern = [];
-    }
-  } else {
-    sounds("wrong");
-    $("body").addClass("game-over");
-    setTimeout(function() {
-      $("body").removeClass("game-over")
-    }, 200);
-    $("#level-title").text("Game Over, you reached level "+ level+ "!");
-    startOver();
-  }
+	if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+		if (userClickedPattern.length == gamePattern.length) {
+			setTimeout(function() {
+				nextSequence();
+			}, 1000);
+			userClickedPattern = [];
+		}
+	} else {
+		sounds('wrong');
+		$('body').addClass('game-over');
+		setTimeout(function() {
+			$('body').removeClass('game-over');
+		}, 200);
+		$('#level-title').text('Game Over, you reached Level ' + level + '!');
+		startOver();
+	}
 }
 
 function startOver() {
-  gamePattern = [];
-  userClickedPattern = [];
-  level = 0;
-  $(".start-btn").show();
+	gamePattern = [];
+	userClickedPattern = [];
+	level = 0;
+	$('.start-btn').show();
 }
 
 /////////////////////////// EFFECTS
 
 function sounds(i) {
-  var audio = new Audio("sounds/" + i + ".mp3");
-  audio.play();
+	var audio = new Audio('sounds/' + i + '.mp3');
+	audio.play();
 }
 
 function animatePress(currentColor) {
-  $("." + currentColor).addClass("pressed");
-  setTimeout(function() {
-    $("." + currentColor).removeClass("pressed")
-  }, 50);
+	$('.' + currentColor).addClass('pressed');
+	setTimeout(function() {
+		$('.' + currentColor).removeClass('pressed');
+	}, 50);
 }
